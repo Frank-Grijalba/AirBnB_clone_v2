92274cd38729be7f9f492a60c06c0f3c6e0d6888
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-08-26 12:58:49 -0500

    Fix tsk 0, delete the rm

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,7 @@ sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo chown -R "$USER":"$USER" /data/
-rm -rf /data/web_static/releases/test/index.html
+
 echo "
 <html>
   <head>
@@ -20,7 +19,8 @@ echo "
   </body>
 </html>
 " >> /data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
+
 sudo ln -s /data/web_static/releases/test /data/web_static/current
+sudo chown -R "$USER":"$USER" /data/
 sudo sed -i '/listen 80 default_server/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
