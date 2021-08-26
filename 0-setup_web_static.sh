6214541e87f36af53e30813a5c69caf1959589ce
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-08-26 13:19:25 -0500

    Fix tsk 0, shellcheck ok

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
 echo "
 <html>
@@ -19,7 +19,7 @@ echo "
 </html>
 " >> /data/web_static/releases/test/index.html
 
-sudo ln -s /data/web_static/releases/test /data/web_static/current
-sudo chown -R "$USER":"$USER" /data/
+ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '/listen 80 default_server/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
