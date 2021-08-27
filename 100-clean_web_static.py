323d5748da072a3a5683b68bf0014e46f86e7b39
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-08-26 22:31:37 -0500

    fix task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -6,12 +6,10 @@ import os
 def do_clean(number=0):
     if number < 0:
         return None
-    number = 1 if int(number) == 0 else int(number)
-
+    number = 2 if (number == 0 or number == 1) else (number + 1)
     with lcd("./versions"):
         local('ls -t | tail -n +{:d} | xargs rm -rf --'.
               format(number))
-
     with cd("/data/web_static/releases"):
         run('ls -t | tail -n +{:d} | xargs rm -rf --'.
             format(number))
